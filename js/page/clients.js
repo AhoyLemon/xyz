3274355a3b0f1027d40d016869692e5bb2acfdd0
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2023-07-09 15:07:57 -0500

    Much better client site structure

--- js/page/clients.js
+++ js/page/clients.js
@@ -1,13 +1,3 @@
-$('.screenshots').slick({
-  infinite: true,
-  arrows: false,
-  dots: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 5000
-});
-
 tippy('.tooltip', {
   //content: 'My tooltip!',
   allowHTML: true,
