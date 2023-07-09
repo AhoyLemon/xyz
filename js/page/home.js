aa327bcef2c61fcc6d2a74e3f0fae53cc67b788e
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2023-07-09 12:32:27 -0500

    Removed slick on homepage

--- js/page/home.js
+++ js/page/home.js
@@ -1,38 +1,57 @@
-$('.upcoming-conference-carousel').slick({
-  prevArrow: '<button type="button" class="slick-prev slick-arrow">&lt;</button>',
-  nextArrow: '<button type="button" class="slick-next slick-arrow">&gt;</button>',
-  //arrows: false,
-  dots: true,
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 1600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+$('#PreviousConferences').mousedown(function (event) {
+  $(this)
+      .data('down', true)
+      .data('x', event.clientX)
+      .data('scrollLeft', this.scrollLeft)
+      .addClass("dragging");
+    }).mouseup(function (event) {
+        $(this)
+          .data('down', false)
+          .removeClass("dragging");
+    }).mousemove(function (event) {
+        if ($(this).data('down') == true) {
+          // $(this).css('overflow','hidden')
+            this.scrollLeft = $(this).data('scrollLeft') + $(this).data('x') - event.clientX;
+        }
+    }).mouseout(function() {
+      // $(this).css('overflow','auto')
+  });
 
-$('.previous-conference-carousel').slick({
-  prevArrow: '<button type="button" class="slick-prev slick-arrow">&lt;</button>',
-  nextArrow: '<button type="button" class="slick-next slick-arrow">&gt;</button>',
-  //arrows: false,
-  dots: true,
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 980,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false
-      }
-    }
-  ]
-});
+// $('.upcoming-conference-carousel').slick({
+//   prevArrow: '<button type="button" class="slick-prev slick-arrow">&lt;</button>',
+//   nextArrow: '<button type="button" class="slick-next slick-arrow">&gt;</button>',
+//   //arrows: false,
+//   dots: true,
+//   infinite: true,
+//   slidesToShow: 2,
+//   slidesToScroll: 2,
+//   responsive: [
+//     {
+//       breakpoint: 1600,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
+// });
+
+// $('.previous-conference-carousel').slick({
+//   prevArrow: '<button type="button" class="slick-prev slick-arrow">&lt;</button>',
+//   nextArrow: '<button type="button" class="slick-next slick-arrow">&gt;</button>',
+//   //arrows: false,
+//   dots: true,
+//   infinite: true,
+//   slidesToShow: 2,
+//   slidesToScroll: 2,
+//   responsive: [
+//     {
+//       breakpoint: 980,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         dots: false
+//       }
+//     }
+//   ]
+// });
